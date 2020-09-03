22cddb425940b34edf8f2b0ce0929f161bcd8f64
jim4067 <jimmyimpulse2@gmail.com>
2020-09-04 00:57:54 +0300

    changed default font family to alata

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './App';
+import reducer from './reducers/anecdoteReducer';
+import './index.css';
 
 const store = createStore(reducer)
 
