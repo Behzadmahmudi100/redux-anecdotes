997877dc750a713d066ffeb7ccda70c6d20101f6
jim4067 <jimmyimpulse2@gmail.com>
2020-09-16 19:14:52 +0300

    feat: using the thunk middleware for async functions

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 import anecdoteReducer from './reducers/anecdoteReducer';
 import filterReducer from './reducers/filterReducer';
@@ -7,13 +8,15 @@ import notificationReducer from './reducers/notificationReducer';
 
 const reducer = combineReducers({
     anecdotes: anecdoteReducer,
-    notification : notificationReducer,
-    filter : filterReducer
+    notification: notificationReducer,
+    filter: filterReducer
 });
 
 const store = createStore(
     reducer,
-    composeWithDevTools()
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
 );
 
 export default store;
